2bb425410d2366b243829bc938a6fd290d5b099a
cafecitoroot <cafecitoroot@gmail.com>
2023-03-17 03:25:50 -0400

    back to next/image

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
